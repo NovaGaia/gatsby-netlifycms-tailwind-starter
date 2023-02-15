fc88cb1db372d40c171083b556d30e7f81ea04f7
Renaud Heluin <renaud.heluin@novagaia.fr>
2023-02-15 15:25:57 +0100

    refactor NAvigationPreview

--- src/cms/preview-templates/NavigationPreview.js
+++ src/cms/preview-templates/NavigationPreview.js
@@ -1,8 +1,37 @@
 import React from 'react'
 
+const DisplayLink = ({ link, key }) => {
+  console.log(link)
+  return (
+    <li
+      key={key}
+      style={{
+        border: 'solid 1px',
+        borderRadius: '.2rem',
+        padding: '1rem',
+        display: 'flex',
+        gap: '.5rem',
+        flexDirection: 'column',
+      }}
+    >
+      <div>
+        <strong>Label</strong>:{' '}
+        <span style={{ wordBreak: 'break-word' }}>{link.name}</span>
+      </div>
+      <div>
+        <strong>Title</strong>:{' '}
+        <span style={{ wordBreak: 'break-word' }}>{link.title}</span>
+      </div>
+      <div>
+        <strong>Url</strong>:{' '}
+        <span style={{ wordBreak: 'break-word' }}>{link.url}</span>
+      </div>
+    </li>
+  )
+}
+
 function NavigationPreview({ entry, getAsset }) {
   const data = entry.getIn(['data']).toJS()
-  console.log(data)
   if (data) {
     return (
       <>
@@ -18,29 +47,7 @@ function NavigationPreview({ entry, getAsset }) {
           <cite>In header bar</cite>
           <ul style={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
             {data.navPrimary?.map((item, key) => {
-              return (
-                <li
-                  key={key}
-                  style={{
-                    border: 'solid 1px',
-                    borderRadius: '.2rem',
-                    padding: '1rem',
-                    display: 'flex',
-                    gap: '.5rem',
-                    flexDirection: 'column',
-                  }}
-                >
-                  <div>
-                    <strong>Label</strong>: <span style={{ wordBreak: 'break-word' }}>{item.name}</span>
-                  </div>
-                  <div>
-                    <strong>Title</strong>: <span style={{ wordBreak: 'break-word' }}>{item.title}</span>
-                  </div>
-                  <div>
-                    <strong>Url</strong>: <span style={{ wordBreak: 'break-word' }}>{item.url}</span>
-                  </div>
-                </li>
-              )
+              return <DisplayLink link={item} key={key} />
             })}
           </ul>
         </section>
@@ -82,38 +89,7 @@ function NavigationPreview({ entry, getAsset }) {
                     }}
                   >
                     {item?.links?.map((subItem, subKey) => {
-                      return (
-                        <li
-                          key={subKey}
-                          style={{
-                            border: 'solid 1px',
-                            borderRadius: '.2rem',
-                            padding: '1rem',
-                            display: 'flex',
-                            gap: '.5rem',
-                            flexDirection: 'column',
-                          }}
-                        >
-                          <div>
-                            <strong>Label</strong>:{' '}
-                            <span style={{ wordBreak: 'break-word' }}>
-                              {subItem.name}
-                            </span>
-                          </div>
-                          <div>
-                            <strong>Title</strong>:{' '}
-                            <span style={{ wordBreak: 'break-word' }}>
-                              {subItem.title}
-                            </span>
-                          </div>
-                          <div>
-                            <strong>Url</strong>:{' '}
-                            <span style={{ wordBreak: 'break-word' }}>
-                              {subItem.url}
-                            </span>
-                          </div>
-                        </li>
-                      )
+                      return <DisplayLink link={subItem} key={subKey} />
                     })}
                   </ul>
                 </li>
